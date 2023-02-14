9879592fccab47c6581b6eb473196aa103cf0d04
groot96 <104350826+groot96@users.noreply.github.com>
2023-02-14 20:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ function minCostToFormRope(event) {
      var cost =0;
 	while(arr.length>1){
 		var res = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0,2);
+		
 		arr.push(res);
 		cost += res;
 		// arr.shift();
 		// arr.shift();
-		arr.splice(0,2);
+		
 
 		arr.sort(function (a,b) {return a-b});
 	}
